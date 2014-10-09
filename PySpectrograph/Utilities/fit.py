97a2b6e383d72c0d70403ee1e771a043b6f52710
cmccully <cmccully@physics.rutgers.edu>
2014-10-09 07:34:23 -0700

    Updated fit.py to use Iterative Reweighting Least Squares instead of normal least squares

--- PySpectrograph/Utilities/fit.py
+++ PySpectrograph/Utilities/fit.py
@@ -11,24 +11,21 @@ import numpy as np
 from scipy import optimize, interpolate
 from scipy.special import legendre, chebyt
 
-
 class power:
-
     """A class to produce a polynomial term of power n.
 
        This has similar behavior to scipy.special.legendre
 
     """
-
     def __init__(self, n):
-        self.n = n
+        self.n=n
 
     def __call__(self, x):
-        return x ** self.n
+        return x**self.n
+
 
 
 class Parameter:
-
     def __init__(self, value):
         self.value = value
 
@@ -38,23 +35,20 @@ class Parameter:
     def __call__(self):
         return self.value
 
-
-def fit(function, parameters, y, x=None, var=1, warn=False):
+def fit(function, parameters, y, x = None, var=1, warn=False):
     def f(params):
         i = 0
         for p in parameters:
             p.set(params[i])
             i += 1
-        return (y - function(x)) / var
+        return (y - function(x))/var
 
-    if x is None:
-        x = np.arange(y.shape[0])
+    if x is None: x = np.arange(y.shape[0])
     p = [param() for param in parameters]
     return optimize.leastsq(f, p, full_output=1, warning=warn)
 
 
 class curfit:
-
     """Given an x and y data arrays,  find the best fitting curve
 
     * x - list or array of x data
@@ -67,31 +61,30 @@ class curfit:
 
 
     """
-
     def __init__(self, x, y, yerr=None, coef=None, function='poly', order=3):
 
-        # set up the variables
-        self.x = x
-        self.y = y
+        #set up the variables
+        self.x=x
+        self.y=y
         if yerr is None:
-            self.yerr = 1
+            self.yerr=1
         else:
-            self.yerr = yerr
-        self.order = order
+            self.yerr=yerr
+        self.order=order
 
         self.set_func(function)
         self.set_coef(coef)
 
+
     def set_coef(self, coef=None):
         """Set the coefficients for the fits for poly, legendre, and chebyshev"""
-        if coef is None:
-            coef = np.ones(self.order + 1)
+        if coef is None: coef=np.ones(self.order+1)
         if isinstance(coef, np.ndarray):
-            self.coef = coef
+            self.coef=coef
         elif isinstance(coef, list):
-            self.coef = np.array(coef)
+            self.coef=np.array(coef)
         else:
-            self.coef = np.array([coef])
+            self.coef=np.array([coef])
 
     def set_func(self, function):
         """Set the function that will be used.
@@ -99,75 +92,94 @@ class curfit:
 
         It will throw an error if an inappropriate function is given
         """
-        self.function = function
-        if self.function == 'poly' or self.function == 'polynomial' or self.function == 'power':
-            self.func = power
-        elif self.function == 'legendre':
-            self.func = legendre
-        elif self.function == 'chebyshev':
-            self.func = chebyt
-        elif self.function == 'spline':
-            self.func = None
+        self.function=function
+        if self.function=='poly' or self.function=='polynomial' or self.function=='power':
+            self.func=power
+        elif self.function=='legendre':
+            self.func=legendre
+        elif self.function=='chebyshev':
+            self.func=chebyt
+        elif self.function=='spline':
+            self.func=None
         else:
-            msg = '%s is not a valid function' % self.function
+            msg='%s is not a valid function' % self.function
             raise Exception(msg)
 
     def set_weight(self, err):
         """Set the weighting for spline fitting """
         if isinstance(err, np.ndarray):
-            if err.any() != 0:
-                self.weight = 1 / err
+            if err.any()!=0:
+                self.weight=1/err
                 return
-        self.weight = None
+        self.weight=None
 
     def __call__(self, x):
         """Return the value of the function evaluated at x"""
-        if self.function == 'spline':
+        if self.function=='spline':
             return interpolate.splev(x, self.coef, der=0)
-        v = x * 0.0
-        for i in range(self.order + 1):
-            v += self.coef[i] * self.func(i)(x)
+        v=x*0.0
+        for i in range(self.order+1):
+            v += self.coef[i]*self.func(i)(x)
         return v
 
     def erf(self, coef, x, y, v):
         """Error function to be minimized in least-squares fit"""
         self.set_coef(coef)
-        return (y - self.__call__(x)) / v
+        return  (y-self.__call__(x))/v
+
+
+    def erf_weights(self, coef, x, y, v, weights=None):
+        """Error function to be minimized in least-squares fit that includes weights for the points."""
+        if weights is None:
+            weights = np.ones(x.shape)
+
+        self.set_coef(coef)
+        return weights * (y - self.__call__(x)) / v
 
     def sigma(self, x, y):
         """Return the RMS of the fit """
-        return (((y - self(x)) ** 2).mean()) ** 0.5
-
+        #Calculate the Absolute median deviation instead
+        return (((y-self(x))**2).mean())**0.5
+        #return np.median(np.abs(y - self(x)))
     def chisq(self, x, y, err):
         """Return the chi^2 of the fit"""
-        return (((y - self(x)) / err) ** 2).sum()
+        return (((y-self(x))/err)**2).sum()
 
     def fit(self, task=0, s=None, t=None, full_output=1, warn=False):
         """Fit the function to the data"""
         if self.function == 'spline':
             self.set_weight(self.yerr)
-            self.results = interpolate.splrep(
-                self.x,
-                self.y,
-                w=self.weight,
-                task=0,
-                s=None,
-                t=None,
-                k=self.order,
-                full_output=full_output)
+            self.results = interpolate.splrep(self.x, self.y, w=self.weight, task=0,
+                                            s=None, t=None, k=self.order, full_output=full_output)
             # w=None, k=self.order, s=s, t=t, task=task,
             # full_output=full_output)
             self.set_coef(self.results[0])
-
         else:
-            self.results = optimize.leastsq(self.erf, self.coef,
-                                            args=(self.x, self.y, self.yerr),
-                                            full_output=full_output)
+            # Fit using the Iterated Reweighted Least Squares Method so that
+            #we are robust to outliers
+            # Initialize the weights to 1's
+            weights = np.ones(len(self.x))
+            # Do 4 iterations for now
+            for i in range(4):
+                # do a linear least squares fit
+                self.results = optimize.leastsq(self.erf_weights, self.coef,
+                                              args=(self.x, self.y, self.yerr, weights),
+                                              full_output=full_output)
+                self.set_coef(self.results[0])
+                # Recalculate the weights (using the biweights)
+                # Start with the residuals
+                r = (self.y - self.__call__(self.x)) / self.yerr
+                # calculate the median absolute deviation
+                # and normalize it to 50% confidence level (0.6745 sigma for a gaussian)
+                s = np.median(abs(r - np.median(r))) / 0.6745
+                biweight = lambda x: ((abs(x) < 1.0) * (1.0 - x ** 2) ** 2.0) ** 0.5
+                weights = biweight(r / s)
+                # We could update p0 to p1 but I would worry that could put us in a
+                # strange part of chi^2 space.
+
             self.set_coef(self.results[0])
 
-
 class interfit(curfit):
-
     """Given an x and y data arrays,  find the best fitting curve.
         After the initial fit, iterate on the solution to reject any
         points which are away from the solution
@@ -184,44 +196,67 @@ class interfit(curfit):
 
 
     """
-
     def __init__(self, x, y, yerr=None, coef=None, function='poly', order=3,
                  thresh=3, niter=5):
-        # set up the variables
-        self.x_orig = x
-        self.y_orig = y
-        self.npts = len(self.x_orig)
+        #set up the variables
+        self.x_orig=x
+        self.y_orig=y
+        self.npts=len(self.x_orig)
         if yerr is None:
-            self.yerr_orig = np.ones(self.npts)
+            self.yerr_orig=np.ones(self.npts)
         else:
-            self.yerr_orig = yerr
+            self.yerr_orig=yerr
 
-        self.order = order
-        self.thresh = thresh
-        self.niter = niter
+        self.order=order
+        self.thresh=thresh
+        self.niter=niter
 
         self.set_func(function)
         self.set_coef(coef)
         self.set_mask(init=True)
         self.set_arrays(self.x_orig, self.y_orig, self.mask, err=self.yerr_orig)
+   
+    def fitfun(self,f,p0,x,y):
+        #Initialize the weights to 1's
+        weights = np.ones(len(x))
+        thiserrfunc = lambda p: weights * (y - f(p,x))
+        #for each iteration
+        for i in range(4):
+            #do a linear least squares fit
+            p1,success = optimize.leastsq(thiserrfunc, p0 )
+            #calcualte the biweights
+            #residuals
+            r = y - f(p1,x)
+            #calculate the median absolute deviation and normalize it to 50% confidence level (6745 sigma for a gaussian)
+            s = np.median(abs(r - np.median(r))) / 0.6745 
+            biweight = lambda x: ((abs(x)<1.0)*(1.0 - x**2)**2.0)**0.5
+            weights = biweight(r/s)
+        return p1
 
     def set_mask(self, init=False):
         """Set the mask according to the values for rejecting points"""
-        self.mask = np.ones(self.npts, dtype=bool)
-        if init:
-            return
+        self.mask=np.ones(self.npts, dtype=bool)
+        if init: return
 
-        # difference the arrays
-        diff = self.y_orig - self(self.x_orig)
-        sigma = self.sigma(self.x, self.y)
-        self.mask = (abs(diff) < self.thresh * sigma)
+        #difference the arrays
+        diff=self.y-self(self.x)
+        if False:
+            f = lambda p,x: p[0]+p[1]* x + p[2]*x*x
+            p1 = self.fitfun(f,[0.0,0.0,0.0],self.x,diff)
+            sigma = np.median(np.abs(diff - f(p1,self.x)))
+        else:
+            sigma = self.sigma(self.x,self.y)
+        self.mask=(abs(diff)<self.thresh*sigma)
 
+ 
+    
     def set_arrays(self, x, y, mask, err=None):
         """set the arrays using a mask"""
-        self.x = x[mask]
-        self.y = y[mask]
+        self.x=x[mask]
+        self.y=y[mask]
         if err is not None:
-            self.yerr = err[mask]
+            self.yerr=err[mask]
+
 
     def interfit(self):
         """Fit a function and then iterate it to reject possible outlyiers"""
